fb5ffb3e1b17f1fa26947ca2b251def0d7963566
Emil Scoffone <emil@scoffone.com>
2020-02-29 02:55:21 -0800

    Bumped version number

--- lib/fl/google/version.rb
+++ lib/fl/google/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Google
     # The version number of the fl-google gem.
 
-    VERSION = '0.1.0'
+    VERSION = '0.1.1'
 
     # The version date for the fl-google gem.
 
-    DATE = '2018-04-27'
+    DATE = '2020-01-19'
   end
 end
